5a5f339485aac39d3ddcfbd178955eb45e921ecf
MUKILTEO\1725205 <1725205@mukilteo.wednet.edu>
2026-03-16 13:36:26 -0700

    Dice Changes

--- Assets/Alex/Scripts/RollDice.cs
+++ Assets/Alex/Scripts/RollDice.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public class RollDice : MonoBehaviour
 {
-    private Transform dice;
     private Rigidbody rb;
     [SerializeField] private float diceSpinCooldown;
     [SerializeField] private Transform diceCamera;
@@ -12,27 +11,31 @@ public class RollDice : MonoBehaviour
 
     [SerializeField] private List<Transform> dices = new();
     private Dictionary<Transform, Rigidbody> diceRB = new();
+    private bool calculated;
+    private float timeSinceCalc;
+    private float calcCooldown = .25f;
 
 
     void Start()
     {
-        
+
         for (int i = 0; i < dices.Count; i++)
         {
             dices[i].gameObject.GetComponent<MeshRenderer>().enabled = false;
             diceRB.Add(dices[i], dices[i].GetComponent<Rigidbody>());
         }
         rb = gameObject.GetComponent<Rigidbody>();
-        dice = gameObject.transform;
     }
 
     void Update()
     {
-        if (follow)
+        timeSinceCalc -= Time.deltaTime;
+        if (!calculated && timeSinceCalc <= 0f && CalculateSpeed() >= .5f)
         {
-           diceCamera.position = new Vector3(dice.position.x, diceCamera.position.y, dice.position.z); 
+            Debug.Log("test");
+            calculated = true;
+            ReadFaces();
         }
-       //
     }
 
     public void AHHAGBAH()
@@ -40,19 +43,47 @@ public class RollDice : MonoBehaviour
         StartCoroutine(Roll());
     }
 
-    IEnumerator Roll()
+    private float CalculateSpeed()
+    {
+        timeSinceCalc = calcCooldown;
+        float speed = 0f;
+
+        foreach (var dice in dices)
+        {
+            speed += diceRB[dice].linearVelocity.magnitude;
+        }
+
+        return speed;
+    }
+
+    IEnumerator Roll() //Make this seeded
     {
         for (int i = 0; i < dices.Count; i++)
         {
             dices[i].gameObject.GetComponent<MeshRenderer>().enabled = true;
             dices[i].position = new Vector3(i - 2f, -2f, i - 2f);
-            dices[i].Rotate( new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f)));
+            dices[i].Rotate(new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f)));
             diceRB[dices[i]].linearVelocity = new Vector3(Random.Range(-8f, 8f), 0f, Random.Range(-8f, 8f));
             diceRB[dices[i]].angularVelocity = new Vector3(Random.Range(-8f, 8f), Random.Range(-8f, 8f), Random.Range(-8f, 8f));
             yield return new WaitForSeconds(diceSpinCooldown);
         }
 
     }
+
+    private void ReadFaces()
+    {
+        string[] faces = new string[dices.Count];
+        for (int i = 0; i < dices.Count; i++)
+        {
+
+        }
+    }
+
+
+
+
+
+
 
 
 
