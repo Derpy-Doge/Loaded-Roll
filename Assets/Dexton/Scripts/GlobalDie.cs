3c7261ffc18f3a219b8613713552c64810c691f0
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2026-03-18 12:08:44 -0700

    gerf🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈🐈‍

--- Assets/Dexton/Scripts/GlobalDie.cs
+++ Assets/Dexton/Scripts/GlobalDie.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class GlobalDie : MonoBehaviour
 {
     public static GlobalDie Instance { get; private set; }
+
     //Place in save data
     public AYellowpaper.SerializedCollections.SerializedDictionary<Vector3, Face> Faces;
+
     private void Awake()
     {
         Instance = this;
