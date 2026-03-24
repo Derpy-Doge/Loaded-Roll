b1b0999e0a064650913cfbe46ce7df18ed42de5a
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2026-03-24 11:20:15 -0700

    tomas von stamplestem

--- Assets/Dexton/Scripts/FaceChange.cs
+++ Assets/Dexton/Scripts/FaceChange.cs
@@ -11,7 +11,6 @@ public class FaceChange : MonoBehaviour
         if (Dice != null)
         {
             UpdateDiceFaces();
-            
         }
     }
     private void Update()
@@ -20,11 +19,11 @@ public class FaceChange : MonoBehaviour
 
     public void UpdateDiceFaces()
     {
-        _faces.materials[0].SetTexture("_BaseMap", Dice.Faces[Vector3.down].Texture);
-        _faces.materials[1].SetTexture("_BaseMap", Dice.Faces[Vector3.up].Texture);
+        _faces.materials[0].SetTexture("_BaseMap", Dice.Faces[Vector3.forward].Texture); //eeee
+        _faces.materials[1].SetTexture("_BaseMap", Dice.Faces[Vector3.down].Texture);
         _faces.materials[2].SetTexture("_BaseMap", Dice.Faces[Vector3.left].Texture);
-        _faces.materials[3].SetTexture("_BaseMap", Dice.Faces[Vector3.right].Texture);
-        _faces.materials[4].SetTexture("_BaseMap", Dice.Faces[Vector3.forward].Texture); //eeee
-        _faces.materials[5].SetTexture("_BaseMap", Dice.Faces[Vector3.back].Texture); 
+        _faces.materials[3].SetTexture("_BaseMap", Dice.Faces[Vector3.back].Texture); 
+        _faces.materials[4].SetTexture("_BaseMap", Dice.Faces[Vector3.right].Texture);
+        _faces.materials[5].SetTexture("_BaseMap", Dice.Faces[Vector3.up].Texture);
     }
 }
