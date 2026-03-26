377ac35333c585db7a598097fd9a0eb31b0d1eb5
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2026-03-26 13:39:25 -0700

    ballltro

--- Assets/Dexton/Scripts/Face.cs
+++ Assets/Dexton/Scripts/Face.cs
@@ -3,7 +3,9 @@ using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "Face", menuName = "Scriptable Objects/Face")]
 public class Face : ScriptableObject
-{   
+{
+    private DiceScoreCalc diceScoreCalc;
+
     public Texture2D Texture;
 
     public int pips, price;
@@ -11,4 +13,19 @@ public class Face : ScriptableObject
     [TextArea]public string Description;
 
     public UnityEvent Effect;
+
+
+    public void Reroll()
+    {
+        // Reroll this die
+    }
+
+    public void Cash(int profit)
+    {
+        diceScoreCalc.points += profit;
+    }
+    public void Choice()
+    {
+        // Get to choose a face on a diffrent die
+    }
 }
