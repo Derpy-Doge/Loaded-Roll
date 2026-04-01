54e4bfb7556f5da18783991a78aa9f329d4d24d3
MUKILTEO\1725577 <kbw2205@icloud.com>
2026-04-01 12:31:49 -0700

    changed some preoject settings and stole a scene from alex for scene :man_juggling:

--- Assets/Kaitlyn/scripts/Save Data/SaveDataController.cs
+++ Assets/Kaitlyn/scripts/Save Data/SaveDataController.cs
@@ -17,6 +17,18 @@ public class SaveDataController : MonoBehaviour
     {
         _instance = this;
         Load();
+        if (current.run.Deese.Count < 1)
+        {
+            var test = new AYellowpaper.SerializedCollections.SerializedDictionary<Vector3, Face>();
+            test[Vector3.up] = Resources.Load<Face>("ScriptableObjects/Faces/2 Pips");
+            test[Vector3.down] = Resources.Load<Face>("ScriptableObjects/Faces/1 Pip");
+            test[Vector3.left] = Resources.Load<Face>("ScriptableObjects/Faces/3 Pips");
+            test[Vector3.right] = Resources.Load<Face>("ScriptableObjects/Faces/4 Pips");
+            test[Vector3.forward] = Resources.Load<Face>("ScriptableObjects/Faces/5 Pips");
+            test[Vector3.back] = Resources.Load<Face>("ScriptableObjects/Faces/6 Pips");
+
+            current.run.Deese.Add(test);
+        }
     }
     //
     private void OnDestroy()
@@ -32,5 +44,25 @@ public class SaveDataController : MonoBehaviour
     public void Load()
     {
         current = Serializer.Load(_saveDataAsset.SaveData, Path.Combine(Application.persistentDataPath, _directory), _fileName);
+    }
+
+    [ContextMenu("Print Dice")]
+    public void PrintDice()
+    {
+        int i = 0;
+        int j = 0;
+
+        foreach (var die in SaveDataController.Instance.current.run.Deese)
+        {
+            i++;
+            Debug.Log($"Dice: {i}");
+            foreach (var key in die.Keys)
+            {
+                j++;
+                Debug.Log($"Face {j}: {die[key]}");               
+            }
+            Debug.Log("\n");
+
+        }
     }
 }
