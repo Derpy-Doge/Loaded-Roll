b35c33da2ca50c5cf2d5719c93cb323fe6438ba6
MUKILTEO\1702073 <1702073@mukilteo.wednet.edu>
2026-04-14 13:34:18 -0700

    I like ye cut gEeEeeEeeEeeeeeeEeee

--- Assets/Dexton/Scripts/Die_Selection.cs
+++ Assets/Dexton/Scripts/Die_Selection.cs
@@ -1,40 +1,38 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Die_Select : MonoBehaviour
+public class Die_Selection : MonoBehaviour
 {
-    public FaceChange faceChange;
-
-    public GlobalDie[] dice;
-
-    public GameObject diceSelection, buyButton;
+    private int _flib = 0;
+    public GameObject buyButton, selectionArea;
+    FaceChange[] faceChange;
+    GlobalDie[] dice;
 
     public void Awake()
     {
-        diceSelection = this.gameObject;
         buyButton = Resources.Load<GameObject>("Prefabs/BuyButton");
-
-        faceChange = FindFirstObjectByType<FaceChange>();
+        selectionArea = this.gameObject;
+        faceChange = FindObjectsByType<FaceChange>(FindObjectsSortMode.None);
         dice = FindObjectsByType<GlobalDie>(FindObjectsSortMode.None);
     }
 
-    public void Start()
+    void Start()
     {
         foreach (GlobalDie die in dice)
         {
-            GameObject instance = Instantiate(buyButton, diceSelection.transform);
+            GameObject instance = Instantiate(buyButton, selectionArea.transform);
 
             instance.GetComponentInChildren<TMPro.TMP_Text>().SetText(die.name);
             instance.GetComponent<Button>().onClick.AddListener(() => ChangeDie(die));
-            instance.GetComponent<Button>().onClick.AddListener(() => faceChange.UpdateDiceFaces());
+            instance.GetComponent<Button>().onClick.AddListener(() => faceChange[_flib].UpdateDiceFaces());
+            _flib++;
         }
     }
+
     public void ChangeDie(GlobalDie newDie)
-    { 
+    {
         // Change all refereces to Global die to the new die
-        FaceChange faceChange = FindFirstObjectByType<FaceChange>();
+        FaceChange faceChange = this.gameObject.GetComponent<FaceChange>();
         faceChange.Dice = newDie;
-
-
     }
 }
